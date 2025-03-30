309b1f8211f980dc2afd8c36feeb1fbf2601ef61
Hassaaan1010 <115409272+Hassaaan1010@users.noreply.github.com>
2025-03-31 01:51:09 +0530

    Delete conf.d/virtualfish-loader.fish

--- conf.d/virtualfish-loader.fish
+++
@@ -1,4 +0,0 @@
-set -g VIRTUALFISH_VERSION 2.5.9
-set -g VIRTUALFISH_PYTHON_EXEC /home/hassaan/.pyenv/versions/3.10.13/bin/python3.10
-source /home/hassaan/.pyenv/versions/3.10.13/lib/python3.10/site-packages/virtualfish/virtual.fish
-emit virtualfish_did_setup_plugins
